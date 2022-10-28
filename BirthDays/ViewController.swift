b8e93be05ed2d02601d47dd9bb6ce62feb253c7d
novexex <nnovexx@gmail.com>
2022-10-28 17:21:44 +0300

    dopilil proverki

--- BirthDays/ViewController.swift
+++ BirthDays/ViewController.swift
@@ -12,19 +12,37 @@ class ViewController: UIViewController {
     @IBOutlet weak var infoLabel: UILabel!
     //@IBOutlet weak var resultButton: UIButton!
     private var daysNumbers = ""
+    private var senderDate = Date()
+    private var nowDate = Date()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        //resultButton.layer.cornerRadius = 12
     }
 
     @IBAction func datePicker(_ sender: UIDatePicker) {
-        let range  = sender.date..<Date.now
-        daysNumbers = range.formatted(.components(style: .wide, fields: [.day]))
+        senderDate = sender.date
+        nowDate = Date.now
     }
     
     @IBAction func resultButtonTapped() {
-            infoLabel.text = "You've been enjoying life for \(daysNumbers)"
+        if senderDate > nowDate {
+            showAlert(title: "Wrong date", message: "You cannot be born in the future")
+            infoLabel.text = "You've been enjoying life for ... days now"
+        } else {
+            let range  = senderDate..<nowDate
+            daysNumbers = range.formatted(.components(style: .wide, fields: [.day]))
+            let number = daysNumbers.split(separator: " ")
+            infoLabel.text = "You've been enjoying life for \(number[0]) days now"
+        }
     }
 
+}
+
+extension ViewController {
+    private func showAlert(title: String, message: String) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "OK", style: .default)
+        alert.addAction(okAction)
+        present(alert, animated: true)
+    }
 }
